2548cef5126c4e4e3c433e414ffc16d2b202b43a
Michael Tsitrin <michael@dymension.xyz>
2023-07-09 21:35:56 +0300

    added scripts to register to local hub

---
+++ scripts/settlement/register_sequencer_to_hub.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+EXECUTABLE="rollappd"
+ROLLAPP_CHAIN_ID="demo-dymension-rollapp"
+KEYRING_PATH="$HOME/.rollapp/sequencer_keys"
+KEY_NAME_SEQUENCER="sequencer"
+
+
+#Register Sequencer
+DESCRIPTION="{\"Moniker\":\"myrollapp-sequencer\",\"Identity\":\"\",\"Website\":\"\",\"SecurityContact\":\"\",\"Details\":\"\"}";
+SEQ_PUB_KEY="$($EXECUTABLE dymint show-sequencer)"
+
+dymd tx sequencer create-sequencer "$SEQ_PUB_KEY" "$ROLLAPP_CHAIN_ID" "$DESCRIPTION" \
+  --from "$KEY_NAME_SEQUENCER" \
+  --keyring-dir "$KEYRING_PATH" \
+  --keyring-backend test \
+  --broadcast-mode block
