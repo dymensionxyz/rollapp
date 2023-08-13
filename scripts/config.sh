9679408875182978abf458793f25d93d5915049f
Michael Tsitrin <michael@dymension.xyz>
2023-08-13 14:43:02 +0300

    added ibc scripts. changed scripts to support env variables

---
+++ scripts/config.sh
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------------------------- #
+#                                ROLLAPP CONFIG                                #
+# ---------------------------------------------------------------------------- #
+ROLLAPP_CHAIN_ID="demo-dymension-rollapp"
+KEY_NAME_ROLLAPP="rol-user"
+DENOM="urax"
+MONIKER=$ROLLAPP_CHAIN_ID-sequencer
